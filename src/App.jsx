c268c7215780d4d8d8586247243cc0cf84ad728b
Sourya07 <singhsourya137@gmail.com>
2025-01-03 22:45:08 +0530

    token routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Key from './key';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import WalletButtons from './component/wallet';
 import { ShowSolBalance } from './Balance';
+import { TokenLaunchpad } from './component/TokenGenrator';
 
 
 
@@ -21,6 +22,7 @@ function App() {
           <Route path="/about" element={< Key />} />
           <Route path="/contact" element={<Key />} />
           <Route path="/Balance" element={<ShowSolBalance />} />
+          <Route path="/token" element={<TokenLaunchpad />} />
         </Routes>
 
 
