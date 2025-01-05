8daec33c1d3382ea3c94e7bcdac42c4e9ab5f465
Sourya07 <singhsourya137@gmail.com>
2025-01-05 22:27:22 +0530

    adding css for token

--- src/component/TokenGenrator/index.jsx
+++ src/component/TokenGenrator/index.jsx
@@ -90,7 +90,7 @@ export function TokenLaunchpad() {
         flexDirection: 'column'
     }}>
         <h1>Solana Token Launchpad</h1>
-        <input className='inputText' type='text' placeholder='Name'></input> <br />
+        <input className='inputText ' type='text' placeholder='Name'></input> <br />
         <input className='inputText' type='text' placeholder='Symbol'></input> <br />
         <input className='inputText' type='text' placeholder='Image URL'></input> <br />
         <input className='inputText' type='text' placeholder='Initial Supply'></input> <br />
